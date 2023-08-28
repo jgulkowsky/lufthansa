93f64ca77516dd2bc4197e9aec72a0d53d7db51c
jgulkowsky <jgulkowsky@gmail.com>
2023-08-28 12:32:55 +0200

    git init / xcode project init / add gitignore

---
+++ Task/Task/TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskApp.swift
+//  Task
+//
+//  Created by Jan Gulkowski on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
